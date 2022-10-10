88e501999689e416053a9d40a92086817c6ecbdc
willy <willangongales@gmail.com>
2022-10-09 21:20:13 -0600

    Proyect_Admin1

--- JS/admin.js
+++ JS/admin.js
@@ -1,7 +1,7 @@
  document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 
  function mostrar_menu(){
-    
+
     document.querySelector(".menu").classList.toggle("mostrar_menu");
  }
 
@@ -9,6 +9,9 @@ window.onscroll = function (){
     var posicion = window.pageYOffset || document.documentElement.scrollTop;
     var will = document.getElementById("icon_heart");
     var luz  = document.getElementById("icon_fire");
-    will.style.bottom = posicion * 0.1 + "px";
-    luz.style.top = posicion * 0.1 + "px"
+    will.style.bottom = posicion * 0.2 + "px";
+    luz.style.top = posicion * 0.2 + "px"
 }
+
+
+
